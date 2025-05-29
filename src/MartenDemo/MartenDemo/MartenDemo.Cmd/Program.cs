512a9ee935a2f873d544a7fd602db46af9f306c6
Hannes Lowette <lowette.hannes@gmail.com>
2025-05-29 02:38:03 +0200

    2.7 - Projecting events

--- src/MartenDemo/MartenDemo/MartenDemo.Cmd/Program.cs
+++ src/MartenDemo/MartenDemo/MartenDemo.Cmd/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Marten;
+using Marten.Events.Projections;
 
 var connectionString = """
                        User ID=postgres;
@@ -17,9 +18,11 @@ var store = DocumentStore.For(options =>
     options.Connection(connectionString);
 
     options.Schema.For<BoxAggregate>().Identity(p => p.BoxId);
+    
+    options.Projections.Add<BeerCountProjection>(ProjectionLifecycle.Inline);
 });
 
-await using var session = store.LightweightSession();
+await using var session = store.IdentitySession();
 
 var stream = session.Events.StartStream<BoxAggregate>(boxId, new BoxCreated(boxId));
 
@@ -34,6 +37,8 @@ await using var session2 = store.LightweightSession();
 
 var aggregate = await session2.Events.AggregateStreamAsync<BoxAggregate>(boxId, version: 6);
 
+var beerCount = await session2.LoadAsync<BeerCount>(boxId);
+
 Console.WriteLine("Done.");
 
 public record BoxCreated(Guid BoxId);
@@ -46,4 +51,28 @@ public record BoxAggregate(Guid BoxId, string[] Beers)
 
     public static BoxAggregate Apply(BeerAdded beerAdded, BoxAggregate previous)
         => previous with { Beers = previous.Beers.Append(beerAdded.BeerName).ToArray() };
+}
+
+public class BeerCountProjection : EventProjection
+{
+    public BeerCountProjection()
+    {
+        Project<BoxCreated>((e, operations) =>
+        {
+            operations.Store(new BeerCount { Id = e.BoxId });
+        });
+        
+        ProjectAsync<BeerAdded>(async (e, operations) =>
+        {
+            var beerCount = await operations.LoadAsync<BeerCount>(e.BoxId);
+            beerCount.NumberOfBeers++;
+            operations.Store(beerCount);
+        });
+    }
+}
+
+public class BeerCount
+{
+    public Guid Id { get; set; }
+    public int NumberOfBeers { get; set; }
 }
