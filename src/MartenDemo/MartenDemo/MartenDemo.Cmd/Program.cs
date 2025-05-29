a756300fa99fd6dcf932f6ca919591f2c5d5e2da
Hannes Lowette <lowette.hannes@gmail.com>
2025-05-29 02:37:16 +0200

    2.6 - Applying events to an aggregate

--- src/MartenDemo/MartenDemo/MartenDemo.Cmd/Program.cs
+++ src/MartenDemo/MartenDemo/MartenDemo.Cmd/Program.cs
@@ -15,11 +15,13 @@ var boxId = Guid.NewGuid();
 var store = DocumentStore.For(options =>
 {
     options.Connection(connectionString);
+
+    options.Schema.For<BoxAggregate>().Identity(p => p.BoxId);
 });
 
 await using var session = store.LightweightSession();
 
-var stream = session.Events.StartStream(boxId, new BoxCreated(boxId));
+var stream = session.Events.StartStream<BoxAggregate>(boxId, new BoxCreated(boxId));
 
 for (int i = 0; i < 10; i++)
 {
@@ -28,9 +30,20 @@ for (int i = 0; i < 10; i++)
 
 await session.SaveChangesAsync();
 
+await using var session2 = store.LightweightSession();
+
+var aggregate = await session2.Events.AggregateStreamAsync<BoxAggregate>(boxId, version: 6);
+
 Console.WriteLine("Done.");
 
 public record BoxCreated(Guid BoxId);
 public record BeerAdded(Guid BoxId, string BeerName);
 
-public record BoxAggregate(Guid BoxId, string[] Beers);
+public record BoxAggregate(Guid BoxId, string[] Beers)
+{
+    public static BoxAggregate Create(BoxCreated created)
+        => new BoxAggregate(created.BoxId, []);
+
+    public static BoxAggregate Apply(BeerAdded beerAdded, BoxAggregate previous)
+        => previous with { Beers = previous.Beers.Append(beerAdded.BeerName).ToArray() };
+}
